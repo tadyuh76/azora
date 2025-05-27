0193e75f0c9af4397a67f631d56cc8cfed601dc1
Tad Yuh <tadyuh76@gmail.com>
2025-05-27 20:29:58 +0700

    add app settings to git ignore

--- obj/Debug/net8.0/AvaloniaAzora.AssemblyInfo.cs
+++ obj/Debug/net8.0/AvaloniaAzora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaAzora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d966215c199d16c4b62189ee73b836d591852d44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1198b40b3809508c9763027856b81596faee5b63")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaAzora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaAzora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
